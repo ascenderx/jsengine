d7701edc5ac17bf253b6edf1ab1c503f19384bc0
James Downer <jamesdanieldowner@gmail.com>
2017-08-28 23:22:11 -0700

    Move ANSI to .java; consolidate prompt functions

--- functions.js
+++ functions.js
@@ -1,15 +1,6 @@
-var ANSI_CLEAR   = '\033c';
-var ANSI_RED     = '\033[1;31m';
-var ANSI_GREEN   = '\033[1;32m';
-var ANSI_YELLOW  = '\033[1;33m';
-var ANSI_BLUE    = '\033[1;34m';
-var ANSI_MAGENTA = '\033[1;35m';
-var ANSI_CYAN    = '\033[1;36m';
-var ANSI_WHITE   = '\033[1;37m';
-var ANSI_RESET   = '\033[0m';
-var stdin        = java.lang.System['in'];
-var stdscan      = new java.util.Scanner(stdin);
-var stdout       = java.lang.System['out'];
+var stdin   = java.lang.System['in'];
+var stdscan = new java.util.Scanner(stdin);
+var stdout  = java.lang.System['out'];
 
 function print(string)
 {
@@ -32,9 +23,42 @@ function prompt(msg)
    print(msg);
    
    print(ANSI_GREEN);
+   var input = read();
+   print(ANSI_RESET);
+
+   return input;
+}
+
+function promptLine(msg)
+{
+   print(msg);
+   
+   print(ANSI_GREEN);
    var input = readline();
    print(ANSI_RESET);
 
+   return input;
+}
+
+function promptInt(msg)
+{
+   print(msg);
+   
+   print(ANSI_GREEN);
+   var input = readint();
+   print(ANSI_RESET);
+   
+   return input;
+}
+
+function promptFloat(msg)
+{
+   print(msg);
+   
+   print(ANSI_GREEN);
+   var input = readfloat();
+   print(ANSI_RESET);
+   
    return input;
 }
 
@@ -48,42 +72,17 @@ function read()
    return stdscan.next();
 }
 
-function readbool()
-{
-   return stdscan.nextBool();
-}
-
-function readbyte()
-{
-   return stdscan.nextByte();
-}
-
-function readdouble()
-{
-   return stdscan.nextDouble();
-}
-
 function readfloat()
 {
-   return stdscan.nextFloat();
+   return parseFloat(stdscan.next());
 }
 
 function readint()
 {
-   return stdscan.nextInt();
+   return parseInt(stdscan.next());
 }
 
 function readline()
 {
    return stdscan.nextLine();
-}
-
-function readlong()
-{
-   return stdscan.nextLong();
-}
-
-function readshort()
-{
-   return stdscan.nextShort();
 }
